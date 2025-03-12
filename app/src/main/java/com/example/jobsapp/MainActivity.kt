e7f698c2cd5bdbcd7d36e750f0b218797a96b389
nurlan <nurkenzebekovkn@gmail.com>
2025-03-12 16:58:38 +0600

    Иконки пока что не меняются корректно

--- app/src/main/java/com/example/jobsapp/MainActivity.kt
+++ app/src/main/java/com/example/jobsapp/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.forEach
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.jobsapp.databinding.ActivityMainBinding
+import com.example.presentation.R as PresentationR
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +26,21 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.menu.forEach { item ->
+                binding.bottomNavigation.invalidate()
+                when (item.itemId) {
+                    PresentationR.id.mainFragment -> item.setIcon(
+                        if (item.itemId == destination.id) PresentationR.drawable.ic_search_blue else PresentationR.drawable.ic_search_grey
+                    )
+                    PresentationR.id.favoritesFragment -> item.setIcon(
+                        if (item.itemId == destination.id) PresentationR.drawable.ic_heart_blue else PresentationR.drawable.ic_heart_grey
+                    )
+                }
+            }
+        }
 
         binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
